9efd558fec9cdbff3d8f0e36cebe11f15fe09ba8
Nino K <helsincraft@gmail.com>
2024-01-28 17:22:25 +0200

    qol improvements to xilliam's id page

--- id/xilliam/index.js
+++ id/xilliam/index.js
@@ -8,10 +8,10 @@ document.getElementById("info").style.top = "30" + "px";
 document.getElementById("info").style.left = "50" + "px";
 
 document.getElementById("cow").style.top = "600" + "px";
-document.getElementById("cow").style.left = "80" + "px";
+document.getElementById("cow").style.left = "60" + "px";
 
-document.getElementById("donate").style.top = "500" + "px";
-document.getElementById("donate").style.left = "700" + "px";
+document.getElementById("donate").style.top = "25" + "px";
+document.getElementById("donate").style.left = "970" + "px";
 
 
 
